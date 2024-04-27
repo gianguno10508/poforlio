6dcbda99352589238b884e8c3513b930bd7a2783
GiangNV <giangnv@arrowhitech.co>
2024-04-27 19:43:28 +0700

    fiigdfhfh

--- index.js
+++ index.js
@@ -1,85 +1,83 @@
-(function() {
-    "use strict";
+(function () {
+  "use strict";
 
-    // define variables
-    var items = document.querySelectorAll(".timeline li");
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
 
-    // check if an element is in viewport
-    // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
-    function isElementInViewport(el) {
-        var rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
     }
+  }
 
-
-
-    function callbackFunc() {
-        for (var i = 0; i < items.length; i++) {
-            if (isElementInViewport(items[i])) {
-                items[i].classList.add("in-view");
-            }
-        }
-    }
-
-    // listen for events
-    window.addEventListener("load", callbackFunc);
-    window.addEventListener("resize", callbackFunc);
-    window.addEventListener("scroll", callbackFunc);
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
 })();
 
-
-
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top >= offset && top < offset + height) {
-            // active navbar links
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }
-        // if want to use animation that repeats on scroll use this
-        else {
-            sec.classList.remove('show-animate');
-        }
-    });
-
-    // sticky header
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-    //menuIcon.classList.remove('bx-x');
-    //navbar.classList.remove('active');
-
-    // animation footer on scroll
-    let footer = document.querySelector('footer');
-    let scrollable = document.documentElement.scrollHeight - window.innerHeight;
-    let scrolled = window.scrollY;
-
-    if (Math.ceil(scrolled) === scrollable) {
-        footer.classList.add('show-animate');
-    } else {
-        footer.classList.remove('show-animate');
+    if (top >= offset && top < offset + height) {
+      // active navbar links
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // active sections for animation on scroll
+      sec.classList.add("show-animate");
     }
-}
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
+    }
+  });
+
+  // sticky header
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar links (scroll)
+  //menuIcon.classList.remove('bx-x');
+  //navbar.classList.remove('active');
+
+  // animation footer on scroll
+  let footer = document.querySelector("footer");
+  let scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  let scrolled = window.scrollY;
+
+  if (Math.ceil(scrolled) === scrollable) {
+    footer.classList.add("show-animate");
+  } else {
+    footer.classList.remove("show-animate");
+  }
+};
 
 /*
 const slides = document.querySelectorAll(".slides img");
@@ -121,7 +119,6 @@ function nextSlide(){
 }
 */
 
-
 // const sections = document.querySelectorAll('section[id]')
 
 // function scrollActive() {
@@ -146,89 +143,126 @@ function nextSlide(){
 
 // window.addEventListener('scroll', scrollActive)
 
-
 var typingEffect = new Typed(".typedText", {
-    strings: ["Lucy", "Truong Dieu Anh", "a Senior", "Future Event Planner"],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 2000
-})
-
-jQuery(document).on('click', 'nav.navbar a[href^="#"]', function(event) {
-    event.preventDefault();
-    jQuery('html, body').animate({
-        scrollTop: jQuery(jQuery.attr(this, 'href')).offset().top
-    }, 500);
+  strings: ["Lucy", "Truong Dieu Anh", "a Senior", "Future Event Planner"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 2000,
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var homeContentItems = document.querySelectorAll('.home-content > *');
-    homeContentItems.forEach(function(item, index) {
-        setTimeout(function() {
-            item.classList.add('fade-up');
-        }, index * 500);
-    });
+jQuery(document).on("click", 'nav.navbar a[href^="#"]', function (event) {
+  event.preventDefault();
+  jQuery("html, body").animate(
+    {
+      scrollTop: jQuery(jQuery.attr(this, "href")).offset().top,
+    },
+    500
+  );
+});
+jQuery(document).on("click", ".btn-send", function (event) {
+  event.preventDefault();
+  var name = jQuery("#name").val();
+  var email = jQuery("#email").val();
+  var message = jQuery("#message").val();
+  var templateParams = {
+    from_name: name,
+    to_name: "Lucy Anh",
+    email: email,
+    message: message,
+  };
+
+  emailjs.send("polo", "template_4fclio6", templateParams).then(
+    function (response) {
+      alert("Send mail success!");
+    },
+    function (error) {
+      alert("Send mail error!");
+    }
+  );
 });
 
-document.addEventListener('scroll', function() {
-    var sections = ['about', 'skills', 'projects', 'contact'];
-    sections.forEach(function(sectionId) {
-        var section = document.getElementById(sectionId);
-        if (!section) return;
-            var sectionRect = section.getBoundingClientRect();
-            var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-                if (sectionRect.top < windowHeight && sectionRect.bottom >= 0) {
-                    if (sectionId === 'about') {
-                        section.classList.add('fadedown');
-                    } else {
-                        section.classList.add('fadeup');
-                    }
-                }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  var homeContentItems = document.querySelectorAll(".home-content > *");
+  homeContentItems.forEach(function (item, index) {
+    setTimeout(function () {
+      item.classList.add("fade-up");
+    }, index * 500);
+  });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var slickScript = document.createElement('script');
-    slickScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js';
-    slickScript.onload = function() {
-        $('.my-slider').slick({
+document.addEventListener("scroll", function () {
+  var sections = ["about", "skills", "projects", "contact"];
+  sections.forEach(function (sectionId) {
+    var section = document.getElementById(sectionId);
+    if (!section) return;
+    var sectionRect = section.getBoundingClientRect();
+    var windowHeight =
+      window.innerHeight || document.documentElement.clientHeight;
+    if (sectionRect.top < windowHeight && sectionRect.bottom >= 0) {
+      if (sectionId === "about") {
+        section.classList.add("fadedown");
+      } else {
+        section.classList.add("fadeup");
+      }
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var slickScript = document.createElement("script");
+  slickScript.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js";
+  slickScript.onload = function () {
+    $(".my-slider").slick({
+      centerMode: true,
+      centerPadding: "80px",
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 500,
+      dots: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            arrows: false,
             centerMode: true,
-            centerPadding: '80px',
+            centerPadding: "40px",
             slidesToShow: 3,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 500,
-            dots: true,
-            responsive: [{
-                    breakpoint: 1024,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '40px',
-                        slidesToShow: 3
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        arrows: false,
-                        centerMode: true,
-                        centerPadding: '80px',
-                        slidesToShow: 1
-                    }
-                }
-            ]
-        });
-    };
-    document.head.appendChild(slickScript);
-});
-document.addEventListener('DOMContentLoaded', function() {
-    var menuIcon = document.getElementById('menu-icon');
-    var menu = document.getElementById('main-menu');
-    menuIcon.addEventListener('click', function() {
-        this.classList.toggle('open');
-        menu.classList.toggle('show');
+          },
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "80px",
+            slidesToShow: 1,
+          },
+        },
+      ],
     });
+    $(".timeline-my-slider").slick({
+      centerMode: false,
+      centerPadding: "80px",
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+      speed: 500,
+      dots: false,
+      arrows: false,
+    });
+  };
+  document.head.appendChild(slickScript);
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var menuIcon = document.getElementById("menu-icon");
+  var menu = document.getElementById("main-menu");
+  menuIcon.addEventListener("click", function () {
+    this.classList.toggle("open");
+    menu.classList.toggle("show");
+  });
 });
